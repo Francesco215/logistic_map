170a39e3ac1a762da5925657700e878e22715e2d
Fracesco215 <francesco215@live.it>
2021-06-18 18:28:22 +0200

    Sono riuscito a fare il grafico minchione con d3

---
+++ zigzag.js
@@ -0,0 +1,64 @@
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 700 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("svg")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+
+//Create the data
+
+const logistic = (x,r) => r*x*(1-x);
+
+var zigzag = (x,r,len) =>{
+    var data=[]
+    for (let i=0;i<len;i++){
+        data.push([i*width/len,x*height]);
+        x=logistic(x,r);
+    }
+    return  data
+}
+var sequence = (x,r,len) =>{
+    var data=[]
+    for (let i=0;i<len;i++){
+        data.push([i,x]);
+        x=logistic(x,r);
+    }
+    return  data
+}
+
+
+var len=70;
+data=zigzag(0.3,3.2,len)
+console.log(data)
+
+
+// Add X axis --> it is a date format
+var x = d3.scaleLinear()
+    .domain([0,len])
+    .range([ 0, width ]);
+
+svg.append("g")
+.attr("transform", "translate(0," + height + ")")
+.call(d3.axisBottom(x));
+
+// Add Y axis
+var y = d3.scaleLinear()
+    .domain([0,1])
+    .range([height, 0]);
+svg.append("g")
+.call(d3.axisLeft(y));
+
+// Add the line
+svg.append("path")
+    .data([data])
+    .attr("d",d3.line())
+    .style("fill",'none')
+    .style("stroke",'red')
+    .style("stroke-width","3px");
